0f1c34220171439e4e740ae6cc7e23b29b866bfb
faizanali-official <FaizanAli.mentorsol@gmail.com>
2025-03-07 14:51:51 +0500

    text align

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
 
-  const App:React.FC = () => {
+const App: React.FC = () => {
 
   return (
     <>
-      <h1 className='text-[green]'>Vite + React</h1>  
+      <h1 className='text-[green] font-bold text-center text-2xl py-3'>Vite  React</h1>
     </>
   )
 }
