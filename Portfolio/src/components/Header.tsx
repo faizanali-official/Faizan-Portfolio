e842a602f53ba8f8ebf1a610aa48ef39751cca4f
faizanali-official <FaizanAli.mentorsol@gmail.com>
2025-03-24 10:39:19 +0500

    set the responsive of header

--- Portfolio/src/components/Header.tsx
+++ Portfolio/src/components/Header.tsx
@@ -13,11 +13,11 @@ const Header: React.FC = () => {
         </Link>
         <NavbarToggle onClick={() => setIsOpen(!isOpen)} />
         <NavbarCollapse className={`flex flex-col md:flex-row items-center justify-between gap-6 ${isOpen ? 'block' : 'hidden'} md:flex`}>
-          <Link to="/" className="hover:text-gray-300 pt-2">Home</Link>
-          <Link to="/about" className="hover:text-gray-300 pt-2">About</Link>
-          <Link to="/contact" className="hover:text-gray-300 pt-2">Contact</Link>
+          <Link to="/" className="hover:text-gray-300 pt-2 md:mx-0 mx-auto">Home</Link>
+          <Link to="/about" className="hover:text-gray-300 pt-2 md:mx-0 mx-auto">About</Link>
+          <Link to="/contact" className="hover:text-gray-300 pt-2 md:mx-0 mx-auto">Contact</Link>
           <img
-            className="w-10 h-10 rounded-full object-cover border-2 md:m-0 mt-2 border-gray-400"
+            className="w-10 h-10 rounded-full object-cover border-2 md:m-0 mt-2 md:mx-0 mx-auto border-gray-400"
             src="./src/assets/images/1721731820291.jpg"
             alt="Profile"
           />
