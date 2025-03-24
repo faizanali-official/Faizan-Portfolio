d15ad634cb62b0b9a4cf163428d104b0a2458461
faizanali-official <FaizanAli.mentorsol@gmail.com>
2025-03-24 10:28:35 +0500

    {commit_message}

--- Portfolio/src/components/Header.tsx
+++ Portfolio/src/components/Header.tsx
@@ -8,16 +8,16 @@ const Header: React.FC = () => {
     <div>
       <Navbar fluid className="bg-black text-white">
         <Link to="/" className="flex items-center gap-2">
-          <img src="/assets/icons/favicon-log.svg" className="h-6 sm:h-9" alt="Logo" />
+          <img src="./src/assets/icons/favicon-log.svg" className="h-6 sm:h-9" alt="Logo" />
           <span className="text-xl font-semibold">Portfolio</span>
         </Link>
         <NavbarToggle onClick={() => setIsOpen(!isOpen)} />
-        <NavbarCollapse className={`flex flex-col md:flex-row items-center gap-6 ${isOpen ? 'block' : 'hidden'} md:flex`}>
-          <Link to="/" className="hover:text-gray-300">Home</Link>
-          <Link to="/about" className="hover:text-gray-300">About</Link>
-          <Link to="/contact" className="hover:text-gray-300">Contact</Link>
+        <NavbarCollapse className={`flex flex-col md:flex-row items-center justify-between gap-6 ${isOpen ? 'block' : 'hidden'} md:flex`}>
+          <Link to="/" className="hover:text-gray-300 pt-2">Home</Link>
+          <Link to="/about" className="hover:text-gray-300 pt-2">About</Link>
+          <Link to="/contact" className="hover:text-gray-300 pt-2">Contact</Link>
           <img
-            className="w-10 h-10 rounded-full object-cover border-2 border-gray-400"
+            className="w-10 h-10 rounded-full object-cover border-2 md:m-0 mt-2 border-gray-400"
             src="./src/assets/images/1721731820291.jpg"
             alt="Profile"
           />
